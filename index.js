68a89b4686a519e8b95754029449642ebe0d9962
sangeetadammo09 <sangeetadammu12@gmail.com>
2024-11-23 12:28:12 +0530

    cors added

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/',(req,res)=>{
 //app.use('/api-docs/helpmetutors',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 app.use(express.json());
 app.use(express.urlencoded({ limit: "50mb", parameterLimit: 500000000, extended:true }));
-app.use(cors());
+app.use(cors({origin:'*',credentials:true}));
 
 //static image for user
 app.use('/uploads', express.static('Storage/images'));
